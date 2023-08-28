641fad20864a65b7ddc6777d01166f496d4cd569
Purshotam <pankajpurshotam@gmail.com>
2023-08-28 20:42:38 +0530

    basic functionality done

---
+++ config/mongoose.js
@@ -0,0 +1,23 @@
+
+const mongoose = require('mongoose');
+
+
+//checking error if any
+main().catch(err => console.log(err));
+
+//connecting mangoose
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/ersData');
+
+}
+
+//connecting db to mangoose connection 
+const db = mongoose.connection;
+
+
+//checking successfully connected 
+db.once('open' , function(){
+    console.log('susscessfully connected to the database of rapidNotes');
+})
+
+module.exports = db;
