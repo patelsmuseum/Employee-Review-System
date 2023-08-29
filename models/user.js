2d4b183720f9a75a300042f716df2cd4457aefef
Purshotam <pankajpurshotam@gmail.com>
2023-08-30 00:01:46 +0530

    changes for deployment

--- models/user.js
+++ models/user.js
@@ -30,7 +30,7 @@ const UserSchema = new mongoose.Schema({
     reviewRecivedFrom: [    // recieved review from another people
         {
             type: mongoose.Schema.Types.ObjectId,
-            ref: 'Review',
+            ref: 'Review'
         }
     ]
 }, 
